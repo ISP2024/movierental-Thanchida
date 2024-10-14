6fb832942f45080a3d37d06d2ee21f81c77d378f
Thanchida <thanchida.a@ku.th>
2024-10-14 16:35:03 +0700

    Refactor rental pricing logic to use polymorphism; add PriceStrategy

--- movie.py
+++ movie.py
@@ -1,23 +1,87 @@
+from abc import ABC, abstractmethod
+
+
+class PriceStrategy(ABC):
+
+    @abstractmethod
+    def get_price(self, days: int) -> float:
+        """The price of this movie rental."""
+        pass
+
+    @abstractmethod
+    def get_rental_points(self, days: int) -> int:
+        """The frequent renter points earned for this rental."""
+        pass
+
+
+class ChildrenPrice(PriceStrategy):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def get_price(self, days):
+        amount = 1.5
+        if days > 3:
+            amount += 1.5 * (days - 3)
+        return amount
+
+    def get_rental_points(self, days):
+        return 1
+
+
+class NewRelease(PriceStrategy):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def get_price(self, days):
+        return 3 * days
+
+    def get_rental_points(self, days):
+        return days
+
+
+class RegularPrice(PriceStrategy):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def get_price(self, days):
+        amount = 2.0
+        if days > 2:
+            amount += 1.5 * (days - 2)
+        return amount
+
+    def get_rental_points(self, days):
+        return 1
+
+
 class Movie:
     """
     A movie available for rent.
     """
     # The types of movies (price_code). 
-    REGULAR = 0
-    NEW_RELEASE = 1
-    CHILDRENS = 2
+    REGULAR = RegularPrice()
+    NEW_RELEASE = NewRelease()
+    CHILDRENS = ChildrenPrice()
     
-    def __init__(self, title, price_code):
+    def __init__(self, title, price_strategy):
         # Initialize a new movie. 
         self.title = title
-        self.price_code = price_code
+        self.price_strategy = price_strategy
 
     def get_price_code(self):
         # get the price code
-        return self.price_code
+        return self.price_strategy
     
     def get_title(self):
         return self.title
+
+    def get_price(self, days):
+        return self.price_strategy.get_price(days)
+
+    def get_rental_points(self, days):
+        return self.price_strategy.get_rental_points(days)
     
     def __str__(self):
         return self.title
